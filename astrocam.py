b56c55998c7b26e34effeacdc7516c8dbc509106
Simon Prunet <prunet@gmail.com>
2019-07-12 11:09:35 -1000

    We seem to have a working routine adjust_wcs ! Final results are in adjusted_params, which are in order:
    p0,p2,p3: coefficients of the radial non-linear transform in pixels. p1 is fixed to 1 to avoid degeneracy with PC matrix
    pc1_1,pc1_2,pc2_1,pc2_2
    crval1,crval2
    
    and adjusted_wcs, which is the corresponding linear wcs instance that best fits, together with the radial non-linear transform parameters.

--- astrocam.py
+++ astrocam.py
@@ -13,6 +13,7 @@ import astropy.wcs as wcs
 import numpy as np
 from pyspherematch import spherematch
 from web import query_vizier
+import scipy.optimize as so
 
 class astrometry:
 
@@ -129,93 +130,30 @@ class astrometry:
         :return: refined wcs solution
         '''
 
-        def radius_transform(r,p0,p2,p3):
-            '''
-            This routine contains the non-linear transform on the radius defined in pixel space, relative to the reference
-            pixel.
-            '''
-            res = p0 + r + p2*r**2 + p3*r**3
-            return res
 
-        def transform(xpix,ypix,p0,p2,p3,linwcs):
-            '''
-            This routine will take (xpix,ypix) pixel coordinates (origin=0,0), several astrometric parameters and transform
-            them to ra,dec positions
-            '''
+        # Create linear version of wcs
+        wcs_orig = self.wcs_orig
+        linwcs = create_linear_wcs(wcs_orig)
+        # Initialize parameters
+        init_params = np.zeros(9)
+        init_params[0:2]=0.
+        init_params[3]=linwcs.wcs.pc[0,0]
+        init_params[4]=linwcs.wcs.pc[0,1]
+        init_params[5]=linwcs.wcs.pc[1,0]
+        init_params[6]=linwcs.wcs.pc[1,1]
+        init_params[7]=linwcs.wcs.crval[0]
+        init_params[8]=linwcs.wcs.crval[1]
+        for i in range(9):
+            print ("init_params[%d] = %f"%(i,init_params[i]))
 
-            # Careful, those are defined w.r.t. origin=(1,1), FITS convention
-            crpix1 = wcs.wcs.crpix[0]
-            crpix2 = wcs.wcs.crpix[1]
-            # Compute pixel coordinates relative to reference pixel
-            dxpix = xpix - (crpix1 - 1)
-            dypix = ypix - (crpix2 - 1)
-            # Compute polar coordinates
-            rpix = np.sqrt(dxpix**2+dypix**2)
-            phipix = np.arctan2(dypix,dxpix)
-            # Apply non-linear transform on radius
-            trpix = radius_transform(r,p0,p2,p3)
-            # Compute transformed relative pixel coordinates
-            tdxpix = trpix*np.cos(phipix)
-            tdypix = trpix*np.sin(phipix)
-            # Compute transformed absolute pixel coordinates
-            txpix = tdxpix + (crpix1-1)
-            typix = tdypix + (crpix2-1)
-
-            # Now we apply the (linear) wcs coordinates
-            ra,dec = linwcs.all_pix2world(txpix,typix,0)
-
-            return ra,dec
-
-        def create_linear_wcs(wcs):
-
-            head = wcs.to_header()
-            linwcs = wcs.WCS(head)
-            return linwcs
-
-        def modify_wcs(linwcs,pc1_1,pc1_2,pc2_1,pc_2_2,crval1,crval2):
-
-            wcs = linwcs.copy()
-
-            wcs.wcs.pc[0,0] = pc1_1
-            wcs.wcs.pc[0,1] = pc1_2
-            wcs.wcs.pc[1,0] = pc2_1
-            wcs.wcs.pc[1,1] = pc2_2
-
-            wcs.wcs.crval[0] = crval1
-            wcs.wcs.crval[1] = crval2
-
-            return wcs
-
-        def pix2sky(params,xpix,ypix,linwcs):
-
-            p0=params[0]
-            p2=params[1]
-            p3=params[2]
-            pc1_1=params[3]
-            pc1_2=params[4]
-            pc2_1=params[5]
-            pc2_2=params[6]
-            crval1=params[7]
-            crval2=params[8]
-
-            mywcs = modify_wcs(linwcs,pc1_1,pc1_2,pc2_1,pc2_2,crval1,crval2)
-            return transform(xpix,ypix,p0,p2,p3,mywcs)
-        
-        def residual(params,xpix,ypix,ra_ref,dec_ref,linwcs):
-
-            ra,dec = pix2sky(params,xpix,ypix,linwcs)
-            n = ra.size
-            res = np.zeros(n)
-            for i in range(n):
-                res[i] = _great_circle_distance(ra[i],dec[i],ra_ref[i],dec_ref[i])
-
-            return res
-
-    # Create linear version of wcs
-    wcs_orig = self.wcs_orig
-    linwcs = create_linear_wcs(wcs_orig)
-    # 
-
+        # Now try to mimimize residual
+        fit_res = so.least_squares(residual,init_params,args=(self.xpix_orig,self.ypix_orig,self.ra_ref,self.dec_ref,linwcs),
+                                   max_nfev=30000, method='lm', xtol=1e-15, ftol=1e-15)
+        print '========='
+        print fit_res
+        self.adjusted_params = fit_res.x
+        p = self.adjusted_params
+        self.adjusted_wcs = modify_wcs(linwcs,p[3],p[4],p[5],p[6],p[7],p[8])
 
 def _great_circle_distance(ra1, dec1, ra2, dec2):
     """
@@ -242,3 +180,86 @@ def _great_circle_distance(ra1, dec1, ra2, dec2):
     denom = sin(phis) * sin(phif) + cos(phis) * cos(phif) * cos(dlamb)
     return degrees(arctan2(numer, denom))
 
+
+def radius_transform(r, p0, p2, p3):
+    '''
+    This routine contains the non-linear transform on the radius defined in pixel space, relative to the reference
+    pixel.
+    '''
+    res = p0 + r + p2 * r ** 2 + p3 * r ** 3
+    return res
+
+
+def transform(xpix, ypix, p0, p2, p3, linwcs):
+    '''
+    This routine will take (xpix,ypix) pixel coordinates (origin=0,0), several astrometric parameters and transform
+    them to ra,dec positions
+    '''
+
+    # Careful, those are defined w.r.t. origin=(1,1), FITS convention
+    crpix1 = linwcs.wcs.crpix[0]
+    crpix2 = linwcs.wcs.crpix[1]
+    # Compute pixel coordinates relative to reference pixel
+    dxpix = xpix - (crpix1 - 1)
+    dypix = ypix - (crpix2 - 1)
+    # Compute polar coordinates
+    rpix = np.sqrt(dxpix ** 2 + dypix ** 2)
+    phipix = np.arctan2(dypix, dxpix)
+    # Apply non-linear transform on radius
+    trpix = radius_transform(rpix, p0, p2, p3)
+    # Compute transformed relative pixel coordinates
+    tdxpix = trpix * np.cos(phipix)
+    tdypix = trpix * np.sin(phipix)
+    # Compute transformed absolute pixel coordinates
+    txpix = tdxpix + (crpix1 - 1)
+    typix = tdypix + (crpix2 - 1)
+
+    # Now we apply the (linear) wcs coordinates
+    ra, dec = linwcs.all_pix2world(txpix, typix, 0)
+
+    return ra, dec
+
+
+def create_linear_wcs(inwcs):
+    head = inwcs.to_header()
+    linwcs = wcs.WCS(head)
+    return linwcs
+
+
+def modify_wcs(linwcs, pc1_1, pc1_2, pc2_1, pc2_2, crval1, crval2):
+    twcs = linwcs.copy()
+
+    twcs.wcs.pc[0, 0] = pc1_1
+    twcs.wcs.pc[0, 1] = pc1_2
+    twcs.wcs.pc[1, 0] = pc2_1
+    twcs.wcs.pc[1, 1] = pc2_2
+
+    twcs.wcs.crval[0] = crval1
+    twcs.wcs.crval[1] = crval2
+
+    return twcs
+
+
+def pix2sky(params, xpix, ypix, linwcs):
+    p0 = params[0]
+    p2 = params[1]
+    p3 = params[2]
+    pc1_1 = params[3]
+    pc1_2 = params[4]
+    pc2_1 = params[5]
+    pc2_2 = params[6]
+    crval1 = params[7]
+    crval2 = params[8]
+
+    mywcs = modify_wcs(linwcs, pc1_1, pc1_2, pc2_1, pc2_2, crval1, crval2)
+    return transform(xpix, ypix, p0, p2, p3, mywcs)
+
+
+def residual(params, xpix, ypix, ra_ref, dec_ref, linwcs):
+    ra, dec = pix2sky(params, xpix, ypix, linwcs)
+    n = ra.size
+    res = np.zeros(n)
+    for i in range(n):
+        res[i] = _great_circle_distance(ra[i], dec[i], ra_ref[i], dec_ref[i])
+
+    return res
