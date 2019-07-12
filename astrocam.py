aa8d2d889b6687665711af83854f56f6d55c39d5
Simon Prunet <prunet@cfht.hawaii.edu>
2019-07-12 06:57:36 -1000

    Getting there

--- astrocam.py
+++ astrocam.py
@@ -121,24 +121,124 @@ class astrometry:
         parameters which consist of:
         1) A pixel space polynomial transform r' = p0 + p1*r + p2*r**2 + p3*r**3. For now, we will fix p1=1 since it is
         degenerate with the determinant of the CDij matrix of WCS
-        2) A CDij matrix, consisting of 4 independent elements. These elements allow for a linear transform between the pixel
-        coordinates and the intermediate world coordinate system in the tangent plane.
+        2) A PC_ij matrix, consisting of 4 independent elements. These elements allow for a linear transform between the pixel
+        coordinates and the intermediate world coordinate system in the tangent plane. We will keep the CDELTi equal to 1.
+        PC_ij is chosen rather than CD_ij since astropy.wcs works internally with PC_ij
         3) CRVAL1,2 give the RA, DEC positions corresponding to the reference pixel of coordinates CRPIX1, CRPIX2
         For a total of 3+4+2 = 9 parameters
         :return: refined wcs solution
         '''
 
-        def radius_transform(r,*p):
+        def radius_transform(r,p0,p2,p3):
             '''
             This routine contains the non-linear transform on the radius defined in pixel space, relative to the reference
             pixel.
             '''
-            res = p[0] + r + p[1]*r**2 + p[2]*r**3
+            res = p0 + r + p2*r**2 + p3*r**3
             return res
 
+        def transform(xpix,ypix,p0,p2,p3,linwcs):
+            '''
+            This routine will take (xpix,ypix) pixel coordinates (origin=0,0), several astrometric parameters and transform
+            them to ra,dec positions
+            '''
+
+            # Careful, those are defined w.r.t. origin=(1,1), FITS convention
+            crpix1 = wcs.wcs.crpix[0]
+            crpix2 = wcs.wcs.crpix[1]
+            # Compute pixel coordinates relative to reference pixel
+            dxpix = xpix - (crpix1 - 1)
+            dypix = ypix - (crpix2 - 1)
+            # Compute polar coordinates
+            rpix = np.sqrt(dxpix**2+dypix**2)
+            phipix = np.arctan2(dypix,dxpix)
+            # Apply non-linear transform on radius
+            trpix = radius_transform(r,p0,p2,p3)
+            # Compute transformed relative pixel coordinates
+            tdxpix = trpix*np.cos(phipix)
+            tdypix = trpix*np.sin(phipix)
+            # Compute transformed absolute pixel coordinates
+            txpix = tdxpix + (crpix1-1)
+            typix = tdypix + (crpix2-1)
+
+            # Now we apply the (linear) wcs coordinates
+            ra,dec = linwcs.all_pix2world(txpix,typix,0)
+
+            return ra,dec
+
+        def create_linear_wcs(wcs):
+
+            head = wcs.to_header()
+            linwcs = wcs.WCS(head)
+            return linwcs
+
+        def modify_wcs(linwcs,pc1_1,pc1_2,pc2_1,pc_2_2,crval1,crval2):
+
+            wcs = linwcs.copy()
+
+            wcs.wcs.pc[0,0] = pc1_1
+            wcs.wcs.pc[0,1] = pc1_2
+            wcs.wcs.pc[1,0] = pc2_1
+            wcs.wcs.pc[1,1] = pc2_2
+
+            wcs.wcs.crval[0] = crval1
+            wcs.wcs.crval[1] = crval2
+
+            return wcs
+
+        def pix2sky(params,xpix,ypix,linwcs):
+
+            p0=params[0]
+            p2=params[1]
+            p3=params[2]
+            pc1_1=params[3]
+            pc1_2=params[4]
+            pc2_1=params[5]
+            pc2_2=params[6]
+            crval1=params[7]
+            crval2=params[8]
+
+            mywcs = modify_wcs(linwcs,pc1_1,pc1_2,pc2_1,pc2_2,crval1,crval2)
+            return transform(xpix,ypix,p0,p2,p3,mywcs)
         
+        def residual(params,xpix,ypix,ra_ref,dec_ref,linwcs):
+
+            ra,dec = pix2sky(params,xpix,ypix,linwcs)
+            n = ra.size
+            res = np.zeros(n)
+            for i in range(n):
+                res[i] = _great_circle_distance(ra[i],dec[i],ra_ref[i],dec_ref[i])
+
+            return res
+
+    # Create linear version of wcs
+    wcs_orig = self.wcs_orig
+    linwcs = create_linear_wcs(wcs_orig)
+    # 
 
 
+def _great_circle_distance(ra1, dec1, ra2, dec2):
+    """
+    (Private internal function)
+    Returns great circle distance.  Inputs in degrees.
 
+    Uses vicenty distance formula - a bit slower than others, but
+    numerically stable.
+    """
+    from numpy import radians, degrees, sin, cos, arctan2, hypot
 
+    # terminology from the Vicenty formula - lambda and phi and
+    # "standpoint" and "forepoint"
+    lambs = radians(ra1)
+    phis = radians(dec1)
+    lambf = radians(ra2)
+    phif = radians(dec2)
+
+    dlamb = lambf - lambs
+
+    numera = cos(phif) * sin(dlamb)
+    numerb = cos(phis) * sin(phif) - sin(phis) * cos(phif) * cos(dlamb)
+    numer = hypot(numera, numerb)
+    denom = sin(phis) * sin(phif) + cos(phis) * cos(phif) * cos(dlamb)
+    return degrees(arctan2(numer, denom))
 
